4c812cdd43414239d85cd5808b1f9bd93616b2d9
Dmitriy <dmitriy.linnik1337@mail.ru>
2023-01-22 21:29:39 +0300

    added-meta-to-header

--- src/App.js
+++ src/App.js
@@ -1,8 +1,13 @@
-import { Route, Switch } from "react-router-dom";
+import { Switch } from "react-router-dom";
 import React from "react";
+import Header from "./ui/header";
 
 function App() {
-  return <Switch></Switch>;
+  return (
+    <div>
+      <Header />
+    </div>
+  );
 }
 
 export default App;
