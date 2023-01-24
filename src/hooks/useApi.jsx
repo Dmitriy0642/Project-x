f0545e44ca113c4c1e101f4d01ff8b99679abc9b
Dmitriy <dmitriy.linnik1337@mail.ru>
2023-01-24 16:56:16 +0300

    added-tostify-to-project

--- src/hooks/useApi.jsx
+++ src/hooks/useApi.jsx
@@ -1,13 +1,14 @@
 import axios from "axios";
 import { useEffect } from "react";
+import config from "../config.json";
 
 const UseApi = () => {
-  const getDataInDb =
-    "https://my-project-b2b2a-default-rtdb.europe-west1.firebasedatabase.app.json";
   useEffect(() => {
     const getData = async () => {
-      const data = await axios.get(getDataInDb);
-      console.log(data);
+      try {
+        const { data } = await axios.get(`${config.ApiEndPOint}` + `.json`);
+        const { category, product } = data;
+      } catch {}
     };
     getData();
   }, []);
