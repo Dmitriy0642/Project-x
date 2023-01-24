fb9593c0468aa2b85e797c4d98d51a0bac4b042e
Dmitriy <dmitriy.linnik1337@mail.ru>
2023-01-24 16:48:25 +0300

    start-develop-useApi

--- src/App.js
+++ src/App.js
@@ -2,12 +2,14 @@ import { Switch } from "react-router-dom";
 import React from "react";
 import Header from "./ui/header";
 import Catalog from "./ui/catalog";
+import UseApi from "./hooks/useApi";
 
 function App() {
   return (
     <div>
       <Header />
       <Catalog />
+      <UseApi />
     </div>
   );
 }
--- src/hooks/useApi.jsx
+++ src/hooks/useApi.jsx
@@ -1,3 +1,16 @@
-const UseApi = () => {};
+import axios from "axios";
+import { useEffect } from "react";
+
+const UseApi = () => {
+  const getDataInDb =
+    "https://my-project-b2b2a-default-rtdb.europe-west1.firebasedatabase.app.json";
+  useEffect(() => {
+    const getData = async () => {
+      const data = await axios.get(getDataInDb);
+      console.log(data);
+    };
+    getData();
+  }, []);
+};
 
 export default UseApi;
