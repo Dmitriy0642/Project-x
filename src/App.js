5555f25588d63eb2c5b6a097d63a35d106b5ca20
Dmitriy <dmitriy.linnik1337@mail.ru>
2023-01-25 02:35:53 +0300

    delete-recat-router-6.3.0

--- src/App.js
+++ src/App.js
@@ -1,15 +1,17 @@
-import { Switch } from "react-router-dom";
 import React from "react";
 import Header from "./ui/header";
 import Catalog from "./ui/catalog";
 import { ToastContainer } from "react-toastify";
 import { ApiProvider } from "./hooks/useApi";
+import { Switch, Route } from "react-router-dom";
 function App() {
   return (
     <div>
       <Header />
       <ApiProvider>
-        <Catalog />
+        <Switch>
+          <Route path="/" exact component={Catalog} />
+        </Switch>
       </ApiProvider>
       <ToastContainer />
     </div>
