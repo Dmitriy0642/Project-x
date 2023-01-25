f49db7cef8a3c9b96e7324722c9d8e77d45055e0
Dmitriy <dmitriy.linnik1337@mail.ru>
2023-01-25 04:57:39 +0300

    add-adaptiv-styles-to-form

--- src/common/reviewCardProduct.jsx
+++ src/common/reviewCardProduct.jsx
@@ -1,22 +1,28 @@
-import { useEffect, useState } from "react";
 import { useApi } from "../hooks/useApi";
 import filterOnCategoryToProduct from "../utils/filterOnCategoryToProduct";
 import filtredSoloData from "../utils/filterSingleDataForSingleVieving";
+import styles from "../common/styles.common/reviewCardProduct.module.css";
+
 const ReviewCardProduct = ({ match }) => {
-  const [allCategoryData, setAllCategoryData] = useState(null);
-  const [singleData, setSingleData] = useState(null);
   const data = useApi();
   const name = match.params.name;
   const id = match.params.postId;
 
-  useEffect(() => {
-    setAllCategoryData(filterOnCategoryToProduct(data, `${name}`));
-    if (allCategoryData !== null) {
-      setSingleData(filtredSoloData(allCategoryData, id));
-    }
-  }, []);
-  console.log(singleData);
-  return singleData !== null ? <div></div> : <h2>Lodaing</h2>;
+  const arrOfCategory = filterOnCategoryToProduct(data, `${name}`);
+  const singleData = filtredSoloData(arrOfCategory, id);
+
+  return singleData === undefined ? (
+    <h1>Lodaing...</h1>
+  ) : (
+    <div>
+      {singleData.map((item) => (
+        <div className={styles.wrapper} key={item._id}>
+          <img src={item.img[0]} className={styles.img_product}></img>
+          <h2 className={styles.title_text}>{item.name}</h2>
+        </div>
+      ))}
+    </div>
+  );
 };
 
 export default ReviewCardProduct;
--- src/utils/filterSingleDataForSingleVieving.js
+++ src/utils/filterSingleDataForSingleVieving.js
@@ -1,9 +1,8 @@
 const filtredSoloData = (data, id) => {
   const getSingleData = data.find((obj) => obj._id === id);
-  //   const readyData = [];
-  //   readyData.push(getSingleData);
-  console.log(getSingleData);
-  //   return readyData;
+  const readyData = [];
+  readyData.push(getSingleData);
+  return readyData;
 };
 
 export default filtredSoloData;
