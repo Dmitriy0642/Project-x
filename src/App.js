9f4484ed56b17508ebd35a135052ce0042aea827
Dmitriy <dmitriy.linnik1337@mail.ru>
2023-01-26 21:08:47 +0300

    create-functional-to-bascet

--- src/App.js
+++ src/App.js
@@ -6,9 +6,27 @@ import { ApiProvider, useApi } from "./hooks/useApi";
 import { Switch, Route } from "react-router-dom";
 import ReviewFormCatalog from "./common/reviewFormCatalog";
 import ReviewCardProduct from "./common/reviewCardProduct";
+import config from "../src/config.json";
+import axios, { all } from "axios";
 function App() {
   useEffect(() => {
-    const getAllData = (async) => () => {};
+    const getAllData = async () => {
+      const { data } = await axios.get(`${config.ApiEndPOint}` + `.json`);
+      const { product } = data;
+      const arrayObject = Object.keys(product).map((item) => product[item]);
+      const allSizes = arrayObject.map((item) => (item = item.quantity));
+      const remakeSizes = allSizes.map((obj) => {
+        const remakeValues = obj.map((item) => {
+          return (item = { size: item.size, value: 0 });
+        });
+        return (obj = remakeValues);
+      });
+      const pushNullSizesToArr = arrayObject.map((item, i) => {
+        return (item = { ...item, quantity: remakeSizes[i] });
+      });
+      const dataToFormat = JSON.stringify(pushNullSizesToArr);
+      localStorage.setItem("AllData", dataToFormat);
+    };
     getAllData();
   }, []);
   return (
