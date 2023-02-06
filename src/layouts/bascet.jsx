9b97fde66e975e89b2eca82b074ae1b239c83858
Dmitriy <dmitriy.linnik1337@mail.ru>
2023-02-06 19:22:55 +0300

    create-counter-in-bascet

--- src/layouts/bascet.jsx
+++ src/layouts/bascet.jsx
@@ -14,6 +14,27 @@ const Bascet = () => {
       return item;
     }
   });
+
+  const handleDecrement = (e) => {
+    console.log(e.target.id);
+    // const newQuantity = dataObj.map((item) => {
+    //   if (`${item.size}` === `${e.target.id}`) {
+    //     return (item.value += 1);
+    //   }
+    //   return { size: item.size, value: item.value };
+    // });
+    // const pushDataToLs = parseDataToFormat.map((item) => {
+    //   if (item._id === filterData[0]._id) {
+    //     return filterData[0];
+    //   }
+    //   return item;
+    // });
+    // localStorage.setItem("AllData", JSON.stringify(pushDataToLs));
+  };
+
+  const handleIncrement = (_id) => {
+    console.log(_id);
+  };
   return (
     <div className={styles.main_div}>
       {filterData.map((item) => (
@@ -27,16 +48,31 @@ const Bascet = () => {
             <h2 className={styles.title_product}>Цена товара</h2>
             <h2 className={styles.title_review}>{item.price}$</h2>
           </div>
+          {console.log(item._id)}
           <div className={styles.third_block}>
             <h2 className={styles.title_product}>Размеры товара</h2>
 
             {item.quantity.map((quan) => (
               <div className={styles.button_block} key={quan.size}>
-                <button className={styles.button_selected_value}>+</button>
+                <button
+                  id={quan.size}
+                  className={styles.button_selected_value}
+                  onClick={handleDecrement}
+                >
+                  +
+                </button>
                 <button className={styles.button_sizes}>
                   {quan.size}({quan.value})
                 </button>
-                <button className={styles.button_selected_value}>-</button>
+                <button
+                  id={quan.size}
+                  className={styles.button_selected_value}
+                  onClick={() => {
+                    handleIncrement(quan.size);
+                  }}
+                >
+                  -
+                </button>
               </div>
             ))}
           </div>
