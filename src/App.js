89e917eae9b621c9a7d2855881be9c57831b496e
Dmitriy <dmitriy.linnik1337@mail.ru>
2023-02-20 00:01:37 +0300

    added-bootstrap-to-proj

--- src/App.js
+++ src/App.js
@@ -10,6 +10,7 @@ import useProductbascet from "./functions/useProductToBascet";
 import Bascet from "./layouts/bascet";
 import Login from "./forms/login";
 import Test from "./forms/test";
+
 function App() {
   useProductbascet();
   return (
@@ -19,9 +20,8 @@ function App() {
         <Switch>
           <Route path="/:name/:postId" component={ReviewCardProduct} />
           <Route path="/login/:type?" component={Login} />
-          <Route path="/test" component={Test} />
-          <Route path="/basket" component={Bascet} />
           <Route path="/" exact component={Catalog} />
+          <Route path="/basket" component={Bascet} />
           <Route path="/:name" component={ReviewFormCatalog} />
         </Switch>
       </ApiProvider>
