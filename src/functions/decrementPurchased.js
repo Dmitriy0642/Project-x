b9ff342b62a96fd235ee272038a89418e77757ab
Dmitriy <dmitriy.linnik1337@mail.ru>
2023-03-17 14:16:24 +0300

    end-developing-of-decremented-after-buing-product

---
+++ src/functions/decrementPurchased.js
@@ -0,0 +1,38 @@
+import orderService from "../services/orders.service";
+const decrementPurchased = async (data, quan) => {
+  const initData = await orderService.getInitiProduct().then((res) => {
+    const toFormat = Object.keys(res).map((item) => res[item]);
+    const filtradeDataByBascet = [];
+    if (data) {
+      const filtradeDataByBascetData = toFormat.forEach((item) => {
+        if (item._id === data._id) {
+          return filtradeDataByBascet.push(item);
+        }
+      });
+    }
+    const quantityFromDb = filtradeDataByBascet[0].quantity;
+    const decrementQuan = quantityFromDb.map((item, index) => {
+      if (`${item.size}` === `${quan[index].size}`) {
+        if (quan[index].value > 0) {
+          return {
+            ...item,
+            value: (item.value -= quan[index].value),
+          };
+        }
+      }
+      return item;
+    });
+    const newData = {
+      _id: data._id,
+      category: data.category,
+      firm: data.firm,
+      img: [...data.img],
+      name: data.name,
+      price: data.price,
+      quantity: decrementQuan,
+    };
+    orderService.changesDataProduct(newData);
+  });
+};
+
+export default decrementPurchased;
