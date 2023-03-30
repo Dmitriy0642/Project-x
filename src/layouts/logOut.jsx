de0735c5089c01dc50bb701198622f24b25922ab
Dmitriy <dmitriy.linnik1337@mail.ru>
2023-03-30 18:38:52 +0300

    treatment-errors-in-auth-service and users actions

--- src/layouts/logOut.jsx
+++ src/layouts/logOut.jsx
@@ -1,13 +1,10 @@
 import { useDispatch } from "react-redux";
-import { useHistory } from "react-router-dom";
 import { logOut } from "../store/users";
 
 const LogOut = () => {
-  const history = useHistory();
   const dispatch = useDispatch();
 
   dispatch(logOut());
-  history.push("/");
 };
 
 export default LogOut;
