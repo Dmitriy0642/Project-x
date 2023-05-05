8c9180b101d72da06a55ca28863a44ddf1c71fe6
Dmitriy <dmitriy.linnik1337@mail.ru>
2023-05-05 18:24:02 +0300

    loaded-data-to-mongoose

--- server/startUp/initDataBase.js
+++ server/startUp/initDataBase.js
@@ -1,4 +1,50 @@
+const Product = require("../models/Product.js");
+const Category = require("../models/Category.js");
+
 const productMock = require("../mock/product.json");
 const categoryMock = require("../mock/category.json");
 
-module.exports = () => {};
+module.exports = async () => {
+  const products = await Product.find();
+  if (products.length !== Object.keys(productMock).length) {
+    await createInitialEntity(Product, productMock);
+  }
+
+  const categorys = await Category.find();
+  if (categorys.length !== Object.keys(categoryMock).length) {
+    await createInitialEntityCategory(Category, categoryMock);
+  }
+};
+
+async function createInitialEntity(Model, data) {
+  await Model.collection.drop();
+  return Promise.all(
+    Object.keys(data).map(async (item) => {
+      try {
+        delete data[item]._id;
+        const newItem = Model(data[item]);
+        await newItem.save();
+        return item;
+      } catch (e) {
+        return e;
+      }
+    })
+  );
+}
+
+async function createInitialEntityCategory(Model, data) {
+  await Model.collection.drop();
+  return Promise.all(
+    Object.keys(data).map(async (item) => {
+      try {
+        const id = data[item]._id;
+        const newData = { id, name: data[item].name };
+        const newItem = Model(newData);
+        await newItem.save();
+        return newData;
+      } catch (e) {
+        return e;
+      }
+    })
+  );
+}
