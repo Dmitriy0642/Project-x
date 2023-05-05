018466c2ec27a0da0d3109e569483088e951602c
Dmitriy <dmitriy.linnik1337@mail.ru>
2023-05-05 13:29:05 +0300

    basic-server-started

--- server/app.js
+++ server/app.js
@@ -5,6 +5,15 @@ const config = require("config");
 const app = express();
 const PORT = config.get("port") ?? 8080;
 
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+
+// if (process.env.NODE_ENV === "production") {
+//   console.log("Prodcution");
+// } else {
+//   console.log("development");
+// }
+
 app.listen(PORT, () => {
   console.log(`Server started on port ${PORT}`);
 });
