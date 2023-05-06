f4ecb9f805d22b4bc6ad1dceaa7d7496f8a85644
Dmitriy <dmitriy.linnik1337@mail.ru>
2023-05-06 16:34:01 +0300

    added-new-npm-pacage

--- server/routes/auth.routes.js
+++ server/routes/auth.routes.js
@@ -1,7 +1,26 @@
 const express = require("express");
+const User = require("../models/User");
 const router = express.Router({ mergeParams: true });
 
-router.post("/signUp", async (req, res) => {});
+router.post("/signUp", async (req, res) => {
+  try {
+    const { email, password } = req.body;
+
+    const existingUser = await User.findOne({ email });
+    if (existingUser) {
+      return res.status(400).json({
+        error: {
+          message: "EMAIL_EXIST",
+          code: 400,
+        },
+      });
+    }
+  } catch (e) {
+    res.status(500).json({
+      message: "На сервере произошла ошибка. Попробуйте позже",
+    });
+  }
+});
 
 router.post("/signInWithPassword", async (req, res) => {});
 
