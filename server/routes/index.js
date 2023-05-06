8aceb5bbdd38b787ed1a7055dafa20f33154d2ec
Dmitriy <dmitriy.linnik1337@mail.ru>
2023-05-06 16:25:06 +0300

    created-new-routes

--- server/routes/index.js
+++ server/routes/index.js
@@ -2,9 +2,9 @@ const express = require("express");
 const router = express.Router({ mergeParams: true });
 
 router.use("/auth", require("./auth.routes"));
-// router.use("/order", require("./auth.routes"));
-// router.use("/bascet", require("./auth.routes"));
-// router.use("/salesProduct", require("./auth.routes"));
+router.use("/order", require("./order.routes"));
+router.use("/bascet", require("./bascet.routes"));
+// router.use("/salesProduct", require("./salesProduct.routes"));
 // router.use("/user", require("./auth.routes"));
 router.use("/product", require("./product.routes"));
 router.use("/category", require("./category.routes"));
