a5df4cd6c3b70ac429f854215e1f3d6d568702ab
Dmitriy <dmitriy.linnik1337@mail.ru>
2023-05-07 16:30:31 +0300

    created-middleware

--- server/routes/index.js
+++ server/routes/index.js
@@ -2,10 +2,11 @@ const express = require("express");
 const router = express.Router({ mergeParams: true });
 
 router.use("/auth", require("./auth.routes"));
+router.use("/user", require("./user.routes"));
 // router.use("/order", require("./order.routes"));
 router.use("/bascet", require("./bascet.routes"));
 // router.use("/salesProduct", require("./salesProduct.routes"));
-// router.use("/user", require("./auth.routes"));
+
 router.use("/product", require("./product.routes"));
 router.use("/category", require("./category.routes"));
 
