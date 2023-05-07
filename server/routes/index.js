496daf69be6168fd086f56a7c60850fe092468ff
Dmitriy <dmitriy.linnik1337@mail.ru>
2023-05-08 01:22:45 +0300

    end-developing-routes-product

--- server/routes/index.js
+++ server/routes/index.js
@@ -3,8 +3,8 @@ const router = express.Router({ mergeParams: true });
 
 router.use("/auth", require("./auth.routes"));
 router.use("/user", require("./user.routes"));
-// router.use("/order", require("./order.routes"));
-// router.use("/bascet", require("./bascet.routes"));
+router.use("/order", require("./order.routes"));
+router.use("/bascet", require("./bascet.routes"));
 router.use("/salesProduct", require("./salesProduct.routes"));
 router.use("/product", require("./product.routes"));
 router.use("/category", require("./category.routes"));
--- server/routes/product.routes.js
+++ server/routes/product.routes.js
@@ -12,5 +12,59 @@ router.get("/", async (req, res) => {
     });
   }
 });
+///find by id
+router.get("/:id", async (req, res) => {
+  try {
+    const { id } = req.params;
+    const findById = await Product.findById(id);
+    res.status(200).send(findById);
+  } catch (e) {
+    res.status(500).json({
+      message: "На сервере произошла ошибка. Попробуйте позже",
+    });
+  }
+});
+
+///change by id
+router.patch("/:id", async (req, res) => {
+  try {
+    const { id } = req.params;
+    const findByIdAndChange = await Product.findByIdAndUpdate(id, req.body, {
+      new: true,
+    });
+    res.status(200).send(findByIdAndChange);
+  } catch (e) {
+    res.status(500).json({
+      message: "На сервере произошла ошибка. Попробуйте позже",
+    });
+  }
+});
+
+///create product
+router.post("/", async (req, res) => {
+  try {
+    const data = req.body;
+    const createdProduct = await Product.create(data);
+    res.status(200).send(createdProduct);
+  } catch (e) {
+    res.status(500).json({
+      message: "На сервере произошла ошибка. Попробуйте позже",
+    });
+  }
+});
+
+///delete Product by id
+
+router.delete("/:id", async (req, res) => {
+  try {
+    const { id } = req.params;
+    const findProduct = await Product.findByIdAndRemove(id);
+    res.status(200).send(findProduct);
+  } catch (e) {
+    res.status(500).json({
+      message: "На сервере произошла ошибка. Попробуйте позже",
+    });
+  }
+});
 
 module.exports = router;
