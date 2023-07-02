471303ab420334c4f9d9abdbac0d4ad39c0e5a30
Dmitriy0642 <dmitriy.linnik1337@mail.ru>
2023-07-02 17:04:35 +0200

    fixed-header

--- client/src/ui/header.jsx
+++ client/src/ui/header.jsx
@@ -5,12 +5,16 @@ import icon_search from "../icons/lupa40px.png";
 import styles from "./ui.styles/header.module.css";
 import { Link } from "react-router-dom";
 import Profile from "./profile";
+import { useState } from "react";
 import { useSelector } from "react-redux";
 import { getCurrentUsers } from "../store/users";
 
 const Header = () => {
   const currentUser = useSelector(getCurrentUsers());
-
+  const [isOpen, setOpen] = useState(false);
+  const toggleMenu = () => {
+    setOpen((prevState) => !prevState);
+  };
   return (
     <div className={styles.main_div}>
       <Link to="/">
@@ -21,17 +25,31 @@ const Header = () => {
         <Link to="#">Каталог</Link>
         <Link to="/aboutShop">О нас</Link>
       </ul>
-      <div className={styles.icon_links}>
-        <Link to="/login">
-          <img src={icon_login} alt="" className={styles.img_nav_links} />
-        </Link>
-        <Link to="/">
-          <img src={icon_search} alt="" className={styles.img_nav_links} />
-        </Link>
-        <Link to="/">
-          <img src={icon_bascet} alt="" className={styles.img_nav_links} />
-        </Link>
-      </div>
+      {
+        currentUser === undefined || currentUser === null ?(  
+          <div className={styles.icon_links}>
+          <Link to="/login">
+            <img src={icon_login} alt="" className={styles.img_nav_links} />
+          </Link>
+          <Link to="/">
+            <img src={icon_search} alt="" className={styles.img_nav_links} />
+          </Link>
+          <Link to="/">
+            <img src={icon_bascet} alt="" className={styles.img_nav_links} />
+          </Link>
+        </div>
+        ):(
+          <div className={styles.links}>
+          <Profile />
+          <Link to="/filter">
+            <img src={icon_search} alt="" className={styles.img_nav_links} />
+          </Link>
+          <Link to="/basket">
+            <img src={icon_bascet} alt="" className={styles.img_nav_links} />
+          </Link>
+        </div>
+        )}
+       
     </div>
   );
 };
